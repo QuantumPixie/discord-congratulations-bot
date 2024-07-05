3f7925d0bf2408be10a0e004ca5a588e9f3c2481
QuantumPixie <tatjana.huppertz@gmail.com>
2024-07-05 22:42:25 +0100

    Delete src directory

--- src/utils/mapTypes/mapUser.ts
+++
@@ -1,8 +0,0 @@
-import { Users } from '../../database/types';
-
-export const mapUser = (user: any): Users => {
-  return {
-    ...user,
-    id: user.id as number,
-  };
-};
--- src/validators/messageValidator.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const createMessageSchema = z.object({
-  username: z.string().min(1),
-  sprintCode: z.string().min(1),
-});
